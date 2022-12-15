df3da0f59e9fd8ec4f8adce8fd48700ce3f45728
Karen <karentomedi@gmail.com>
2022-12-15 15:18:36 -0300

    home style done

--- src/pages/Bookings/Horarios.js
+++ src/pages/Bookings/Horarios.js
@@ -1,67 +1,67 @@
-import { useEffect, useState } from "react";
-import { Card, Button, Container, Form } from "react-bootstrap";
-import { api } from "../../api/api";
+// import { useEffect, useState } from "react";
+// import { Card, Button, Container, Form } from "react-bootstrap";
+// import { api } from "../../api/api";
 
 
-function Horarios({ form, setForm }) {
+// function Horarios({ form, setForm }) {
 
-  const [freeHours, setFreeHours] = useState();
-
-
-  useEffect(() => {
-    async function fetchFreeHours() {
-      try {
+//   const [freeHours, setFreeHours] = useState();
 
 
-        const body = {
-          resource: form.resource,
-          schedule: form.data
-        }
+//   useEffect(() => {
+//     async function fetchFreeHours() {
+//       try {
 
-        const response = await api.post("/booking/availability", body);
 
-        setFreeHours(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchFreeHours();
-  }, []);
+//         const body = {
+//           resource: form.resource,
+//           schedule: form.data
+//         }
 
-  console.log(freeHours)
+//         const response = await api.post("/booking/availability", body);
 
-  function handleChange(e) {
-    console.log("handle change do radio ", e);
-    setForm({ ...form, schedule: e.target.id});
-  }
+//         setFreeHours(response.data);
+//         setIsLoading(false);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     }
+//     fetchFreeHours();
+//   }, []);
 
-  return (
-    <div>
-      {/*Escolher o horario*/}
-      <Container className="border rounded mt-3">
-        <h2>Horários disponíveis</h2>
-        <Form>
-        <div key={`default-radio`} className="mb-3">
-        {!isLoading && freeHours
-            .map((hour) => {
-              return (
-                  <Form.Check 
-                    type="radio"
-                    name="hour-radio"
-                    id={`${form.data}-${hour}`}
-                    label={`${hour}`}
-                    onChange={handleChange}
-                  />
-              );
-            })} 
-          </div>      
-        </Form>
-      </Container>
+//   console.log(freeHours)
 
-    </div> 
+//   function handleChange(e) {
+//     console.log("handle change do radio ", e);
+//     setForm({ ...form, schedule: e.target.id});
+//   }
 
-  );
-}
+//   return (
+//     <div>
+//       {/*Escolher o horario*/}
+//       <Container className="border rounded mt-3">
+//         <h2>Horários disponíveis</h2>
+//         <Form>
+//         <div key={`default-radio`} className="mb-3">
+//         {!isLoading && freeHours
+//             .map((hour) => {
+//               return (
+//                   <Form.Check 
+//                     type="radio"
+//                     name="hour-radio"
+//                     id={`${form.data}-${hour}`}
+//                     label={`${hour}`}
+//                     onChange={handleChange}
+//                   />
+//               );
+//             })} 
+//           </div>      
+//         </Form>
+//       </Container>
 
-export default Horarios;
+//     </div> 
+
+//   );
+// }
+
+// export default Horarios;
