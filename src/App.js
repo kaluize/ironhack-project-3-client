d56827fc6ed0d12623522cb423f3486d17608213
Karen <karentomedi@gmail.com>
2022-12-15 20:44:56 -0300

    barra lateral

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/index.js";
-import { Login } from "./pages/Login/index.js";
-import { SignUp } from "./pages/SignUp/index.js";
+// import { Login } from "./pages/Login/index.js";
+// import { SignUp } from "./pages/SignUp/index.js";
 import { AuthContextComponent } from "./contexts/authContext";
 import { Profile } from "./pages/Profile/index.js";
 import { ErrorPage } from "./pages/ErrorPage/index.js";
 import BookingsPage from "./pages/Bookings/index.js";
+// import ConfigurationsPage from "./pages/Configurations/index.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { ProtectedRoute } from "./components/ProtectedRoute/index.js";
-import { ProtectedGestorRoute } from "./components/ProtectedGestorRoute/index.js";
+// import { ProtectedRoute } from "./components/ProtectedRoute/index.js";
+// import { ProtectedGestorRoute } from "./components/ProtectedGestorRoute/index.js";
 
 function App() {
   return (
@@ -17,8 +17,8 @@ function App() {
       <AuthContextComponent>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/signup" element={<Sign-up />} /> */}
           <Route path="/profile/*"
             // element={<ProtectedRoute Component={Profile} />}
             element={<Profile />}/>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,49 +1,48 @@
 import MyBookings from "./MyBookings";
 import MyResources from "./MyResources";
 import { UserInfo } from "./UserInfo";
-import AllUsers from "./AllUsers"
+import AllUsers from "./AllUsers";
 import { ProtectedRoute } from "../../components/ProtectedRoute";
 import { ProtectedGestorRoute } from "../../components/ProtectedGestorRoute";
 import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-
+import "../../app.css";
 
 export function Profile() {
-  
   return (
-    
-    <>
+    <div className="profile-page">
 
-  <div className="barraNav">
-    <h1>Barra de navegação à esquerda e janela com info à direita</h1>
+      <div className="barra-lateral">
+        <div className="avatar">
+        <h1>Fulano</h1>
+        </div>
+        <div>
+            <p><Link className="link-barra-lateral" to="/" >Meus dados</Link></p>
+            <p><Link className="link-barra-lateral" to="/recursos" >Meus recursos</Link></p>
+            <p><Link className="link-barra-lateral" to="/reservas" >Minhas reservas</Link></p>
+            <p><Link className="link-barra-lateral" to="/">Sair</Link></p>
+        </div>
+      </div>
 
-
-
-  </div>
-
-        <Routes>
-          <Route
-            path="/meus-dados"
-            element={<ProtectedRoute Component={UserInfo} />}
-          />
-          <Route
-            path="/todos"
-            element={<ProtectedRoute Component={AllUsers} />}
-          />
-          <Route
-            path="/minhas-reservas"
-            element={<ProtectedRoute Component={MyBookings} />}
-          />
-          <Route
-            path="/meus-recursos"
-            element={<ProtectedRoute Component={MyResources} />}
-          />
-         
-        </Routes>
-
-
-    </>
-
-
+      <Routes>
+        <Route
+          path="/meus-dados"
+          element={<ProtectedRoute Component={UserInfo} />}
+        />
+        <Route
+          path="/todos"
+          element={<ProtectedRoute Component={AllUsers} />}
+        />
+        <Route
+          path="/minhas-reservas"
+          element={<ProtectedRoute Component={MyBookings} />}
+        />
+        <Route
+          path="/meus-recursos"
+          element={<ProtectedRoute Component={MyResources} />}
+        />
+      </Routes>
+    </div>
   );
 }
