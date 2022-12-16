7cc40f9d9e73adabfcf5cf3fb6ce6444c7479e93
Vitor Martins Costa <costa.vsm@gmail.com>
2022-12-16 10:37:14 -0300

    corrected edit user

--- src/pages/Profile/UserInfo/index.js
+++ src/pages/Profile/UserInfo/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import EditMeModal from "../../../components/UserComponents/EditUserModal";
+import EditMeModal from "../../../components/UserComponents/EditMeModal";
 import { api } from "../../../api/api";
 
 export function UserInfo() {
