0d465469bf02a3c583803991559a0d61c1b73ce9
Karen <karentomedi@gmail.com>
2022-12-16 01:22:16 -0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
 import { Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "../app.css"
+import "../app.css";
 
 function NavBar() {
   return (
     <Navbar className="navBar">
-        <div>
-            <Link className="link-navbar" to="/" >Home</Link>
-            <Link className="link-navbar" to="/recursos" >Recursos</Link>
-            <Link className="link-navbar" to="/reservas" >Reservas</Link>
-            <Link className="link-navbar" to="/" >Sair</Link>
-        </div>
+      <div>
+        <Link className="link-navbar" to="/">
+          Home
+        </Link>
+        <Link className="link-navbar" to="/sobre-nos">
+          Sobre nós
+        </Link>
+        <Link className="link-navbar" to="*">
+          Não clique aqui
+        </Link>
+        <Link className="link-navbar" to="/">
+          Sair
+        </Link>
+      </div>
     </Navbar>
   );
 }
